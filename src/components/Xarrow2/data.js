1661ec961cce6879ae7c19df237b247c1f8b182c
abhishekkumar <kumar008abhishek@gmail.com>
2024-08-26 01:00:07 +0530

    added changes for react xarrow

---
+++ src/components/Xarrow2/data.js
@@ -0,0 +1,18 @@
+export const data = [
+  {
+    id: "ele1",
+    name: "Ele 1",
+  },
+  {
+    id: "ele2",
+    name: "Ele 2",
+  },
+  {
+    id: "ele3",
+    name: "Ele 3",
+  },
+  {
+    id: "ele4",
+    name: "Ele 4",
+  },
+];
--- src/components/Xarrow2/index.js
+++ src/components/Xarrow2/index.js
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useRef } from "react";
+import Xarrow, { useXarrow, Xwrapper } from "react-xarrows";
+import Draggable from "react-draggable";
+import { data } from "./data";
+
+const boxStyle = {
+  border: "grey solid 2px",
+  borderRadius: "10px",
+  padding: "5px",
+};
+
+const DraggableBox = ({ id, name }) => {
+  const updateXarrow = useXarrow();
+  return (
+    <Draggable onDrag={updateXarrow} onStop={updateXarrow}>
+      <div id={id} style={boxStyle}>
+        {name}
+      </div>
+    </Draggable>
+  );
+};
 
 const XarrowComponent = () => {
-  return <div>XarrowComponent</div>;
+  return (
+    <div
+      style={{ display: "flex", justifyContent: "space-evenly", width: "100%" }}
+    >
+      <Xwrapper>
+        {data.map((item) => (
+          <DraggableBox id={item.id} name={item.name} />
+        ))}
+        {data.map(
+          (item, i) =>
+            data[i + 1] && (
+              <Xarrow start={item.id} end={data[i + 1].id} showHead={true} />
+            )
+        )}
+      </Xwrapper>
+    </div>
+  );
 };
 
 export default XarrowComponent;
